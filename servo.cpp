340298d6516411e445373d1c0515bb1006c45032
PatriceGreg <134339007+PatriceGreg@users.noreply.github.com>
2023-05-22 19:04:59 -0400

    Update servo.cpp

--- servo.cpp
+++ servo.cpp
@@ -1,2 +1,15 @@
 #include <includes/wiringPi.h>
+#include <softPwm.h>
 
+int main(){
+
+    byte servoPin = 0;
+    pinMode(servoPin, OUTPUT);
+    
+    while(1){
+        digitalWrite(servoPin, LOW);
+        delay(500);
+        digitalWrite(servoPin,HIGH);
+        delay(500);
+    }
+}
